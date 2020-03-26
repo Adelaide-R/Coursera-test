5ad2011e6dbf0e8d32ca53c7d12af1caf4eef975
Rossi Adelaide <UGA05652@direzione.gr-u.it>
2020-03-26 11:46:54 +0100

    fixed typos

--- mod4_solution/SpeakGoodBye.js
+++ mod4_solution/SpeakGoodBye.js
@@ -4,12 +4,11 @@
 // STEP 6: Wrap the entire contents of SpeakGoodBye.js inside of an IIFE
 // See Lecture 52, part 2
 
-(function (window) {
+(function () {
   var byeSpeaker = {};
-  byeSpeaker.speak = function () {
-    var speakWord = "Good Bye";
-  
-    console.log(speakWord + " " + name);
+  var speakWord = "Good Bye";
+  byeSpeaker.speak = function (name) {
+        console.log(speakWord + " " + name);
   }
     window.byeSpeaker = byeSpeaker;
   })(window);
